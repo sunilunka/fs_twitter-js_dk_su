94b523369aaaf01cdc084a040e5d33e2bc75a9d7
Sunil Unka <sunil.unka@gmail.com>
2015-09-23 11:34:23 -0400

    Set up static route for css file.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
+var morgan = require("morgan");
+var path = require("path");
 var tweetBank = require("../tweetBank.js");
 
 router.use(function(req, res, next) {
-  console.log(req.method, req.url, res.statusCode);/*
+  var logger = morgan("combined", {
+    skip: function(req, res) {
+      return res.statusCode < 400;
+    }
+  });
+
+  logger(req, res, function(error){
+    if(error) return done(error);
+  });
+
+  /* console.log(req.method, req.url, res.statusCode);
   console.log("Verb: ",req.method);
   console.log("Path: ",req.url);
   console.log("Status Code: ",res.statusCode);*/
@@ -15,6 +27,27 @@ router.get('/', function(req, res, next){
   res.render('index', { title: 'Twitter.js', tweets: tweets });
   //res.send("website is working  ");
   //people.printPeople();
+});
+
+router.get( "/stylesheets/style.css", function(req, res, next){
+  var options = {
+    root: path.join(__dirname, '../public/stylesheets'),
+    dotfile: "deny",
+    headers: {
+      "x-timestamp" : Date.now(),
+      "x-sent" : true
+    }
+
+  };
+
+  res.sendFile('style.css', options, function(error){
+    if(error){
+      console.log(error);
+      res.status(error.status).end();
+    } else {
+      console.log("Sent: style.css");
+    }
+  });
 });
 
 router.get('/news', function(req, res, next){
